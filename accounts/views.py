0b4fdafab4a72edb637a40d71a39d7df4f9fb4a1
ayyaehab <ayyaehab66@gmail.com>
2021-12-16 17:22:38 +0200

    new token

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from accounts.models import Accounts
+from accounts.serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+
+
+
+class UsersAPI(generics.ListCreateAPIView):
+    queryset = Accounts.objects.all()
+    serializer_class = UserSerializer
+    # permission_classes = [IsAdminUser]
+
+    def get_user(self):
+        user = self.request.user
+        return user
