7baef5a4a0c5ef91e67f07bd8592793c1a54ee01
Mostafa-YMS <mos_302@yahoo.com>
2021-12-20 17:57:04 +0200

    bus location

--- mapapi/views.py
+++ mapapi/views.py
@@ -8,18 +8,21 @@ from rest_framework.response import Response
 @api_view(['POST'])
 def create(request):
         ser = Mapser(data=request.data)
-        if ser.is_valid(): 
+        if ser.is_valid():
              ser.save()
         return Response(ser.data)
            
 @api_view(['POST'])
 def update(request,name):
-    maps = driver.objects.get(name=name)
-
-    ser = Mapser(instance=maps,data=request.data)
-    if ser.is_valid():
-        ser.save()
-    return Response(ser.data)
+    maps = driver.objects.filter(name=name)
+    if not maps:
+        ser = Mapser(data=request.data)
+        if ser.is_valid():
+            print("valid")
+            ser.save()
+        return Response(ser.data)
+    maps.update(latitude=request.data["latitude"], longitude=request.data["longitude"])
+    return Response(200)
 
 @api_view(['GET'])
 def drive_get(request):
